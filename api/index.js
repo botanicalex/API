51dc2e589da6f99f2969b1a0417be65ff8dc763f
botanicalex <alexavascolopera@hotmail.com>
2025-04-30 17:00:57 -0500

    Actualizar mensaje

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 app.get('/', (req, res) => {
-  res.send('Adios Mundo Cruel!');
+  res.send('poppop!');
 });
 
  app.get('/api', (req, res) => {
